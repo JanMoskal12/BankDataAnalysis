43bcf33aca19d97bda77f5fa30ba424327212bbc
przemekq <kubi.prze@gmail.com>
2024-04-10 12:05:43 +0200

    cleaning mess

--- models_comparison.R
+++ models_comparison.R
@@ -1,27 +1,17 @@
----
-title: "Raport"
-format: html
-editor: visual
----
 
-```{r}
 library(tidymodels)
-```
 
-```{r}
 data <- readRDS("Dane_czyste.rds")
 
 set.seed(2000)
 
-#podział danych na zbiór testowy i treingowy
+
 data_split <- initial_split(data, 0.85)
 train <- training(data_split)
 test_data <- testing(data_split)
 
-```
 
-```{r}
-rf_fit <- readRDS("wf_rf_best_fit.rds")
+rf_fit <- readRDS("modele/wf_rf_best_fit.rds")
 
 pred <- predict(rf_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
@@ -36,113 +26,115 @@ rf_metrics
 metric_names <- matrix(c("F1 score", "Accuracy", "Sensitivity", "Specificity", "Recall", "J index"), ncol=1)
 colnames(metric_names) <- c("Metric")
 metric_names <- as_tibble(metric_names)
-metric_names
 
 
-svm_fit <- readRDS("wf_best_svm_fit.rds")
+
+svm_fit <- readRDS("modele/wf_best_svm_fit.rds")
 
 pred <- predict(svm_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
-met <- metric_set(f_meas, accuracy, sensitivity, specificity, recall, j_index)
 svm_metrics <- met(data=pred, truth=y, estimate=.pred_class)
 svm_metrics <- svm_metrics[,3]
 colnames(svm_metrics) <- c("SVM")
 svm_metrics
 
-knn_fit <- readRDS("wf_knn_best_fit.rds")
+knn_fit <- readRDS("modele/wf_knn_best_fit.rds")
 
 pred <- predict(knn_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
 conf_mat_rf <- conf_mat(pred, truth=y, estimate=.pred_class)
-met <- metric_set(f_meas, accuracy, sensitivity, specificity, recall, j_index)
 knn_metrics <- met(data=pred, truth=y, estimate=.pred_class)
 knn_metrics <- knn_metrics[,3]
 colnames(knn_metrics) <- c("KNN")
 knn_metrics
 
 
-tree_fit <- readRDS("wf_tree_best_fit.rds")
+tree_fit <- readRDS("modele/wf_tree_best_fit.rds")
 
 pred <- predict(tree_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
-met <- metric_set(f_meas, accuracy, sensitivity, specificity, recall, j_index)
 tree_metrics <- met(data=pred, truth=y, estimate=.pred_class)
 tree_metrics <- tree_metrics[,3]
 colnames(tree_metrics) <- c("Decision Tree")
 tree_metrics
 
-log_fit <- readRDS("wfbest_regresja_train.rds")
+log_fit <- readRDS("modele/wfbest_regresja_fit.rds")
 
 pred <- predict(log_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
-met <- metric_set(f_meas, accuracy, sensitivity, specificity, recall, j_index)
 log_metrics <- met(data=pred, truth=y, estimate=.pred_class)
 log_metrics <- log_metrics[,3]
 colnames(log_metrics) <- c("Logistic Regression")
 log_metrics
 
 
-boost_fit <- readRDS("wfbest_boosting_fit.rds")
+boost_fit <- readRDS("modele/wfbest_boosting_fit.rds")
 
 pred <- predict(boost_fit, new_data=test_data)
 pred <- cbind(test_data, pred)
-met <- metric_set(f_meas, accuracy, sensitivity, specificity, recall, j_index)
 boost_metrics <- met(data=pred, truth=y, estimate=.pred_class)
 boost_metrics <- boost_metrics[,3]
 colnames(boost_metrics) <- c("Boosting")
 boost_metrics
 
-summary_table <- metric_names %>% 
+summary_table <- metric_names |> 
   cbind(rf_metrics, svm_metrics, knn_metrics, tree_metrics, log_metrics, boost_metrics)
 
-#save(summary_table, file="model_summary_table.rda")
+
 
 summary_table
-```
 
-```{r}
+save(summary_table, file="wykresy/model_summary_table.rda")
+
+
+
+
 
 rf_pred <- predict(rf_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-rf_roc <- rf_pred %>% 
+rf_roc <- rf_pred |>  
   roc_curve(y, .pred_0)
 
 svm_pred <- predict(svm_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-svm_roc <- svm_pred %>% 
+svm_roc <- svm_pred |>  
   roc_curve(y, .pred_0)
 
 knn_pred <- predict(knn_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-knn_roc <- knn_pred %>% 
+knn_roc <- knn_pred |>  
   roc_curve(y, .pred_0)
 
 tree_pred <- predict(tree_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-tree_roc <- tree_pred %>% 
+tree_roc <- tree_pred |> 
   roc_curve(y, .pred_0)
 
 log_pred <- predict(log_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-log_roc <- log_pred %>% 
+log_roc <- log_pred |> 
   roc_curve(y, .pred_0)
 
 boost_pred <- predict(boost_fit, new_data=test_data, type = "prob") %>% 
   bind_cols(test_data)
-boost_roc <- boost_pred %>% 
+boost_roc <- boost_pred |> 
   roc_curve(y, .pred_0)
 
 
 curve <- ggplot()+
-  geom_path(data=rf_roc,aes(x = 1 - specificity, y = sensitivity, col="#e41a1c"))+
+  geom_path(data=rf_roc,aes(x = 1 - specificity, y = sensitivity, col="darkblue"))+
   geom_path(data=svm_roc,aes(x = 1 - specificity, y = sensitivity ,col="#377eb8"))+
   geom_path(data=knn_roc,aes(x = 1 - specificity, y = sensitivity, col="#4daf4a"))+
   geom_path(data=tree_roc,aes(x = 1 - specificity, y = sensitivity, col="#984ea3"))+
   geom_path(data=log_roc, aes(x = 1 - specificity, y = sensitivity, col="#ff7f00"))+
   geom_path(data=boost_roc, aes(x = 1 - specificity, y = sensitivity, col="#ffff33"))+
   scale_color_identity(name = "Model",
-                       breaks = c("#e41a1c", "#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33"),
+                       breaks = c("darkblue", "#377eb8","#4daf4a","#984ea3","#ff7f00","#ffff33"),
                        labels = c("Random Forest","Support Vector Machines","K-nearest Neighbors", "Decision Tree","Logistic Regression","XGBoost"),
-                       guide = "legend")
+                       guide = "legend")+
+  theme_bw()
+
 curve
-```
+
+save(curve, file="wykresy/curve.rda")
+
